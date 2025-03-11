f023730054fb1b035a652c1b22277e1ea15c498b
auuuguuust <augustpicard@gmail.com>
2025-03-11 15:43:07 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const randomImageElement = document.querySelector("#random-image");
 
 let imageArray = [
-    "/images/kat.jpeg",
-    "/images/koeien 50 x 50.jpeg",
-    "/images/lege stippen. 40 x 40 cm.jpeg",
-    "/images/ongespannen 40 x 74.jpeg",
-    "/images/opus 1 - 140 x 130 cm.jpeg",
-    "/images/schaduwbeeldje 2.jpeg"
+    "/yannstruyf/images/kat.jpeg",
+    "/yannstruyf/images/koeien 50 x 50.jpeg",
+    "/yannstruyf/images/lege stippen. 40 x 40 cm.jpeg",
+    "/yannstruyf/images/ongespannen 40 x 74.jpeg",
+    "/yannstruyf/images/opus 1 - 140 x 130 cm.jpeg",
+    "/yannstruyf/images/schaduwbeeldje 2.jpeg"
 ];
 
 function displayImage() {
